67b320d4c0a9ba5a73d3fd3684b8f8c5dae28c48
Jack Norquist <jack.norquist@outlook.com>
2024-02-13 18:44:31 -0600

    Added api key

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+'use strict';
+
+async function getGiph(evt) {
+  evt.preventDefault();
+  let searchTerm = $('.search-term').val();
+  let api_key = '3adru9XXXZKjlPudsw2TH0GCpCjcTiVK';
+  let response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${api_key}`);
+  let test = await response.json();
+}
+
+
+$(".giph-search").on('submit', getGiph);
+
 console.log("Let's get this party started!");
